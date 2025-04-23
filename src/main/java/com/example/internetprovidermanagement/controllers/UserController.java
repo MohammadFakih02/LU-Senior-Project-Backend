89713698c95e6eac0fad9fd6e01173df9dd0d641
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-04-23 12:23:28 +0300

    added delete user to userController

--- src/main/java/com/example/internetprovidermanagement/controllers/UserController.java
+++ src/main/java/com/example/internetprovidermanagement/controllers/UserController.java
@@ -1,16 +1,25 @@
 package com.example.internetprovidermanagement.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.internetprovidermanagement.dtos.CreateUpdateUserDTO;
 import com.example.internetprovidermanagement.dtos.UserDetailsDTO;
 import com.example.internetprovidermanagement.dtos.UserResponseDTO;
 import com.example.internetprovidermanagement.services.UserService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -39,5 +48,10 @@ public class UserController {
             @PathVariable Long id,
             @Valid @RequestBody CreateUpdateUserDTO userDTO) {
         return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
